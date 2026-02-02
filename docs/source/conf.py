78ff91c7fd45347f6a0168cf3610bbfd613995c0
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2026-02-02 16:46:24 +0700

    Change HTML theme from sphinx_rtd_theme to sphinx-book-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = sphinx-book-theme
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
